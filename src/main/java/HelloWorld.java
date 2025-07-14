41a4a88baa6af9df438587463c1d5d2f397fa55e
sanyogitagupta <guptaaashu0513@gmail.com>
2025-07-14 20:20:09 +0530

    Test CI after flattening repo

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins CI!");
+        System.out.println("Update HelloWorld message to trigger CI");
     }
 }
